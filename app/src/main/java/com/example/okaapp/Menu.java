4bffaacc11d8d06174cb1421b8f91de6fb7ccfbd
gedebobby <56390837+gedebobby@users.noreply.github.com>
2020-06-09 12:42:33 +0800

    first commit

---
+++ app/src/main/java/com/example/okaapp/Menu.java
@@ -0,0 +1,34 @@
+package com.example.okaapp;
+
+public class Menu {
+
+    private String nama;
+    private String harga;
+    private String gambar;
+    private String keterangan;
+
+    public Menu(String datanama, String dataharga, String datagambar, String dataketerangan){
+
+        nama = datanama;
+        harga = dataharga;
+        gambar = datagambar;
+        keterangan = dataketerangan;
+
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public String getGambar() {
+        return gambar;
+    }
+}
